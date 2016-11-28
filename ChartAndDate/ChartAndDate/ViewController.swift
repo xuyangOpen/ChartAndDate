4526df27cc0208b213bca7a770998aabc129ec93
胡凡 <hufan@hufandemacbook-pro.local>
2016-11-28 15:35:58 +0800

    添加背景颜色

--- ChartAndDate/ChartAndDate/ViewController.swift
+++ ChartAndDate/ChartAndDate/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
     }
-
+//MARK:
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
